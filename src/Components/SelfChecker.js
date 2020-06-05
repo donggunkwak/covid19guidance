846873c1592a597940922b3c3957e1e0b4a414cd
donggunkwak <donggunkwak@gmail.com>
2020-06-06 07:49:55 +0900

    upload

--- src/Components/SelfChecker.js
+++ src/Components/SelfChecker.js
@@ -8,8 +8,14 @@ const SelfChecker = (props) => {
   return (
     <div id="selfChecker">
       <img src={ Corona } width="40px" alt="corona"></img>
-      <span id="title1">CORONAVIRUS</span>
-      <span id="title2">SELF-CHECKER</span>
+      {props.lang === 'en-us'
+        ? <span id="title1">CORONAVIRUS</span>
+        : <span id="title1-kr">코로나바이러스</span>
+      }
+      {props.lang === 'en-us'
+        ? <span id="title2">SELF-CHECKER</span>
+        : <span id="title2-kr">자가 진단</span>
+      }
       <span id="title3">This self-checker is from the Centers for Disease Control and Prevention.</span>
       <div id="iframe-box">
         <Iframe url={"https://covid19healthbot.cdc.gov/?language=" + props.lang }
--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,18 +1,30 @@
 import React, { useState } from 'react';
 import Header from '../Components/Header';
+import Guidance from '../Components/Guidance';
 import SelfChecker from '../Components/SelfChecker';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
 import './App.scss';
 
 
 const App = () => {
   const [lang, setLang] = useState('eng');
   const [menu, setMenu] = useState(false);
+  const [status, setStatus] = useState('selfChecker') // guidance or selfChecker
 
   const onChangeLanguage = (l) => {
     setLang(l);
   };
   const toggleDrawer = () => {
     setMenu(!menu);
+  };
+  const changeStatus = (s) => {
+    setMenu(false);
+    if (s === 'GUIDANCE') {
+      setStatus('guidance');
+    } else if (s === 'SELF-CHECKER') {
+      setStatus('selfChecker');
+    }
   };
   const parseLang = (l) => {
     if (l === 'kor') {
@@ -28,7 +40,23 @@ const App = () => {
         onChangeLanguage={ onChangeLanguage }
         lang={ lang }
       />
-      <SelfChecker lang={ parseLang(lang) }/>
+      {status === 'guidance' &&
+        <Guidance lang={ lang }/>
+      }
+      {status === 'selfChecker' && 
+        <SelfChecker lang={ parseLang(lang) }/>
+      }
+      <Drawer anchor={ 'bottom' } open={ menu }
+        onClose={ toggleDrawer }>
+        <List>
+          {['SELF-CHECKER', 'GUIDANCE'].map((item, idx) => (
+              // <ListItem button key={item} onClick={ () => changeStatus(item) }>{ item }</ListItem>
+              <div className="list-item" key="item" onClick={ () => changeStatus(item) }><span>{ item }</span>
+                <div className="line"></div>
+              </div>
+          ))}
+        </List>
+      </Drawer>
     </div>
   );
 };
